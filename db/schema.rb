40726f8fe46c85427efd3981992a3c199fe54da5
Akhilesh Patel <akleshrailsdev@gmail.com>
2025-03-27 19:11:16 +0530

    Added models and migrations

---
+++ db/schema.rb
@@ -0,0 +1,52 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[8.0].define(version: 2025_03_27_133925) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
+  create_table "check_invoices", force: :cascade do |t|
+    t.bigint "check_id", null: false
+    t.bigint "invoice_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["check_id"], name: "index_check_invoices_on_check_id"
+    t.index ["invoice_id"], name: "index_check_invoices_on_invoice_id"
+  end
+
+  create_table "checks", force: :cascade do |t|
+    t.string "number"
+    t.bigint "company_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["company_id"], name: "index_checks_on_company_id"
+  end
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "invoices", force: :cascade do |t|
+    t.string "number"
+    t.bigint "company_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["company_id"], name: "index_invoices_on_company_id"
+  end
+
+  add_foreign_key "check_invoices", "checks"
+  add_foreign_key "check_invoices", "invoices"
+  add_foreign_key "checks", "companies"
+  add_foreign_key "invoices", "companies"
+end
